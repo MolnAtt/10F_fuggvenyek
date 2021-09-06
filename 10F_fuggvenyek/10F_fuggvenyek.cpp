64b5754ed2586bf24409d928eb9f7bfe28ee3615
MolnAtt <molnar.attila@szlgbp.hu>
2021-09-06 12:05:27 +0200

    & és * operátorok, csere függvény címekkel és értékekkel

--- 10F_fuggvenyek/10F_fuggvenyek.cpp
+++ 10F_fuggvenyek/10F_fuggvenyek.cpp
@@ -2,7 +2,58 @@
 
 using namespace std;
 
+
+int elso_fuggvenyem(int, int); // függvény deklarációja
+void csere(int*, int*);
+void jobb_csere(int&, int&);
+
 int main()
 {
-    cout << "Hello World!\n";
+    int a,b;
+
+    a = 5;
+    b = 7;
+
+    cout << a << ";" << b << endl;
+ 
+    //csere(&a, &b);
+    jobb_csere(a, b);
+
+    cout << a << ";" << b << endl;
+
+    cout << "POINTEREK\n";
+
+    cout << "a erteke = " <<  a << endl;
+    cout << "a helye= "   << &a << endl;
+
+    int* p = &a; // ez azt jelenti, hogy a p-ben eltároljuk az a helyét
+
+    cout << "p erteke = " << p << endl;
+    cout << "p címen található dolog = " << *p << endl;
+    cout << "p helye= " << &p << " (de ez most igazan nem lenyeges)" <<endl;
+
+    // *p az, ami a memóriacímbõl elõállítja az ott lévõ dolgot
+    // &a az, ami az a változóból elõszedi, hogy az milyen memóriacímen is van.
+
+
+
+}
+
+int elso_fuggvenyem(int a, int b) // függvény definíciója
+{
+    return 2 * a + b;
+}
+
+void csere(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void jobb_csere(int& x, int& y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
 }
